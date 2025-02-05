6ba1d02c1a952d2cfc18672abb0db38c8b7931cb
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-06 01:32:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let prices = document.querySelectorAll(".price");
+	let totalSum = 0;
+	for(let v of prices){
+		totalSum += parseInt(v.innerText)
+	}
+	let table = document.querySelector(".table");
+	let row = document.createElement("tr");
+	let cell1 = document.createElement("td");
+	let cell2 = document.createElement("td");
+
+	cell1.innerText = "Total";
+	cell2.innerText = totalSum;
+
+	row.append(cell1);
+	row.append(cell2);
+	table.append(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
