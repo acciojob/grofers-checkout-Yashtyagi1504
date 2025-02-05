9fa801438d53ea9f6844382151589866d91e6945
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-06 01:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ const getSum = () => {
 	}
 	let table = document.querySelector(".table");
 	let row = document.createElement("tr");
-	let cell1 = document.createElement("td");
+	// let cell1 = document.createElement("td");
 	let cell2 = document.createElement("td");
 
-	cell1.innerText = "Total";
+	// cell1.innerText = "Total";
 	cell2.innerText = totalSum;
 
-	row.append(cell1);
+	// row.append(cell1);
 	row.append(cell2);
 	table.append(row);
 };
